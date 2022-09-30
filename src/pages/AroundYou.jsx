fe17656d28461e672350800e6f688127710299c6
downinahill <brian.hill.0023@gmail.com>
2022-09-30 06:47:11 -0400

    components and pages

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -15,7 +15,7 @@ const CountryTracks = () => {
 
   useEffect(() => {
     axios
-      .get(`https://geo.ipify.org/api/v2/country?apiKey='at_V1nPtmcAu5Ty1SOUvdLNdKGu1k7Kk'`)
+      .get(`https://geo.ipify.org/api/v2/country?apiKey=at_V1nPtmcAu5Ty1SOUvdLNdKGu1k7Kk`)
       .then((res) => setCountry(res?.data?.location.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
@@ -27,7 +27,7 @@ const CountryTracks = () => {
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around you <span className="font-black">{country}</span></h2>
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around the <span className="font-black">{country}</span></h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
